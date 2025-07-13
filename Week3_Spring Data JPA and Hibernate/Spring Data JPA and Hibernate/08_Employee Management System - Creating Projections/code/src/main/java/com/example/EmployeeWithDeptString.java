71c960369084dc9ea2b72f702dfb6a6a87ac2f52
GOKUL K <147173298+GOKUL7387@users.noreply.github.com>
2025-07-13 17:52:57 +0530

    Delete Week3_Spring Data JPA and Hibernate/Spring Data JPA and Hibernate directory

--- Week3_Spring Data JPA and Hibernate/Spring Data JPA and Hibernate/08_Employee Management System - Creating Projections/code/src/main/java/com/example/EmployeeWithDeptString.java
+++
@@ -1,6 +0,0 @@
-import org.springframework.beans.factory.annotation.Value;
-
-public interface EmployeeWithDeptString {
-    @Value("#{target.name + ' works in ' + target.department.name}")
-    String getFormattedInfo();
-}
