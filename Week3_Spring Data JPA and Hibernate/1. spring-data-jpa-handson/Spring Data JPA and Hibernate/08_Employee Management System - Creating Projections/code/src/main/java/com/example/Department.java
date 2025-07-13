d3b13605e0517f013884e52a7b64ad4f5f953782
GOKUL K <147173298+GOKUL7387@users.noreply.github.com>
2025-07-13 17:50:02 +0530

    Delete Week3_Spring Data JPA and Hibernate/1. spring-data-jpa-handson directory

--- Week3_Spring Data JPA and Hibernate/1. spring-data-jpa-handson/Spring Data JPA and Hibernate/08_Employee Management System - Creating Projections/code/src/main/java/com/example/Department.java
+++
@@ -1,9 +0,0 @@
-@Entity
-public class Department {
-    @Id
-    @GeneratedValue
-    private Long id;
-    private String name;
-
-    // Getters and setters
-}
--- Week3_Spring Data JPA and Hibernate/1. spring-data-jpa-handson/Spring Data JPA and Hibernate/08_Employee Management System - Creating Projections/code/src/main/java/com/example/EmployeeNameSalary.java
+++
@@ -1,4 +0,0 @@
-public interface EmployeeNameSalary {
-    String getName();
-    double getSalary();
-}
